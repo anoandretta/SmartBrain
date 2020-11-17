4dea6dbc6a3df60fe1c38f40a6a21720f606daa8
anoandretta <dricaogura@gmail.com>
2020-11-17 22:29:57 +0100

    conecting server

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ displayFaceBox = (box) => {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://secure-crag-71842.herokuapp.com/imageurl', {
           method: 'post',
           headers: { 'Content-Type' : 'application/json'},
           body: JSON.stringify({
@@ -88,7 +88,7 @@ displayFaceBox = (box) => {
         .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://secure-crag-71842.herokuapp.com/image', {
           method: 'put',
           headers: { 'Content-Type' : 'application/json'},
           body: JSON.stringify({
--- src/Component/SignIn/SignIn.js
+++ src/Component/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://secure-crag-71842.herokuapp.com/signin', {
       
       method: 'post',
       headers: { 'Content-Type' : 'application/json'},
